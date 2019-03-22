521746f7741d41d6dd4569a21c6cac08aa64528d
Didik Wicaksono <didik.wicaksono@gmail.com>
2019-03-22 16:29:15 +0700

    WIP octokit

--- lib/sejarah.rb
+++ lib/sejarah.rb
@@ -1,9 +1,33 @@
 require "sejarah/version"
 require "thor"
+require "octokit"
 
 module Sejarah
   class Error < StandardError; end
 
   class CLI < Thor
+    desc "hello", "Call user name from GitHub"
+    def hello
+      repos.each do |repo|
+        merged_issues_from(repo).each do |issue|
+          @releases
+          puts "- [#{issue.closed_at}] #{issue.html_url} #{issue.title}"
+        end
+      end
+    end
+
+    private
+
+      def client
+        @_client ||= Octokit::Client.new(access_token: ENV.fetch("GITHUB_ACCESS_TOKEN"))
+      end
+
+      def merged_issues_from(repo)
+        client.get("search/issues?q=repo:#{repo}+is:merged&sort=updated&order=desc").items
+      end
+
+      def repos
+        ENV.fetch("REPOS").split(",")
+      end
   end
 end
--- sejarah.gemspec
+++ sejarah.gemspec
@@ -39,4 +39,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.17"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_dependency "thor", "~> 0.20"
+  spec.add_dependency "octokit", "~> 4.13"
 end

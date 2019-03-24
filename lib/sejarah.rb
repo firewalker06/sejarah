ed36ee03bde8e4cdb412ee9e73e0b9b3111c884b
Didik Wicaksono <didik.wicaksono@gmail.com>
2019-03-24 16:19:34 +0700

    Write merged pull requests to Markdown file grouped by closed date

--- lib/sejarah.rb
+++ lib/sejarah.rb
@@ -1,33 +1,76 @@
 require "sejarah/version"
 require "thor"
 require "octokit"
+require "date"
 
 module Sejarah
   class Error < StandardError; end
 
   class CLI < Thor
-    desc "hello", "Call user name from GitHub"
-    def hello
+    desc "write", "Group releases from repositories by date"
+    def write
+      @releases = []
+
       repos.each do |repo|
-        merged_issues_from(repo).each do |issue|
-          @releases
-          puts "- [#{issue.closed_at}] #{issue.html_url} #{issue.title}"
+        pages.times.each do |page_number|
+          merged_issues_from(repo, page_number).each do |issue|
+            @releases << {
+              closed_at: issue.closed_at,
+              url: issue.html_url,
+              title: issue.title,
+              repo: repo
+            }
+          end
         end
       end
+
+      write_to_file
     end
 
     private
+
+      attr_accessor :releases
+
+      def write_to_file
+        file = File.new("sejarah.md", "w")
+        file.write(markdown_texts)
+        file.close
+      end
+
+      def markdown_texts
+        text = []
+
+        releases_by_date.sort_by { |s| s.first }.reverse.each do |date, releases|
+          text << "### #{date.to_s}"
+
+          releases.each do |release|
+            text << "- [#{release[:closed_at]}] [#{release[:repo]}] [#{release[:title]}](#{release[:url]})"
+          end
+        end
+
+        text.join("\n")
+      end
+
+      def releases_by_date
+        releases.group_by do |release|
+          release[:closed_at].to_date
+        end
+      end
 
       def client
         @_client ||= Octokit::Client.new(access_token: ENV.fetch("GITHUB_ACCESS_TOKEN"))
       end
 
-      def merged_issues_from(repo)
-        client.get("search/issues?q=repo:#{repo}+is:merged&sort=updated&order=desc").items
+      def merged_issues_from(repo, page_number)
+        client.get("search/issues?q=repo:#{repo}+is:merged&page=#{page_number}").items
       end
 
       def repos
         ENV.fetch("REPOS").split(",")
+      end
+
+      def pages
+        ENV.fetch("PAGES", 3).to_i
       end
   end
 end
